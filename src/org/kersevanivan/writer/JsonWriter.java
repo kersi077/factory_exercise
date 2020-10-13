67bdefeb7c651d90fc1c32eeaba8a0b6caf3ea1f
Ivan Kerševan <Ivan Kersevan@gmx.de>
2020-10-13 14:00:22 +0200

    A working example for a factory.

---
+++ src/org/kersevanivan/writer/JsonWriter.java
@@ -0,0 +1,16 @@
+package org.kersevanivan.writer;
+/**
+ * 
+ * <p>Title: JsonWriter</p>
+ * <p>Description: </p>
+ * <p>Copyright: Copyright (c) 2020 ec-logic GmbH, Fuerth, germany</p>
+ * <p>Company: ec-logic GmbH</p>
+ * <p>$LastChangedRevision: $</p>
+ * <p>$Id: $</p>
+ * <p>$LastChangedDate: $</p>
+ * <p>$HeadURL: $</p>
+ * @author ivan
+ * @version 13.10.20 12:37
+ */
+ public class JsonWriter {
+}
